83b8abf9ec0d15fe6e9964eb81c8fb8e7e19a6a5
Katrinast <stehnovicekaterina@gmail.com>
2024-12-16 00:01:13 +0200

    added google authorization

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import * as authControllers from "../controllers/auth.js"; 
+import * as authControllers from "../controllers/auth.js";
 import ctrlWrapper from "../utils/ctrlWrapper.js";
 import validateBody from "../utils/validateBody.js";
 
-import { authRegisterSchema, authLoginSchema, requestResetEmailSchema } from "../validation/auth.js";
+import { authRegisterSchema, authLoginSchema, requestResetEmailSchema, loginWithGoogleOAuthSchema } from "../validation/auth.js";
 
 import { resetPasswordSchema } from '../validation/auth.js';
 
@@ -16,6 +16,13 @@ authRouter.post("/refresh", ctrlWrapper(authControllers.refreshSessionController
 authRouter.post("/logout", ctrlWrapper(authControllers.logoutController));
 authRouter.post("/send-reset-email", validateBody(requestResetEmailSchema), ctrlWrapper(authControllers.requestResetEmailController));
 authRouter.post('/reset-pwd', validateBody(resetPasswordSchema), ctrlWrapper(authControllers.resetPasswordController),
+);
+authRouter.get('/get-oauth-url', ctrlWrapper(authControllers.getGoogleOAuthUrlController));
+
+authRouter.post(
+  '/confirm-oauth',
+  validateBody(loginWithGoogleOAuthSchema),
+  ctrlWrapper(authControllers.loginWithGoogleController),
 );
 
 
