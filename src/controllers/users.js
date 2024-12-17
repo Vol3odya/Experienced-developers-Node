8e15b1bf7509152e41391c2bdef41320b800aa2a
Katrinast <stehnovicekaterina@gmail.com>
2024-12-17 14:00:51 +0200

    users routers
    added router for update wateRate and for update user's info and router for update only user's photo

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,9 +1,13 @@
 import createHttpError from 'http-errors';
-import { getUserProfile } from '../services/users.js';
+import { getUserProfile, getUserWaterRate, updateUserInfo, updeteUserPhoto } from '../services/users.js';
+import {saveFileToCloudinary} from '../utils/saveFileToCloudinary.js';
+import {saveFileToUploadDir} from '../utils/saveFileToUploadDir.js';
+
+import { env } from '../utils/env.js';
 
 export const getUsersController = async (req, res, next) => {
   const userId = req.user._id;
-  
+
   const userById = await getUserProfile(userId);
 
   if (userById === null) {
@@ -16,3 +20,66 @@ export const getUsersController = async (req, res, next) => {
     data: userById,
   });
 };
+
+
+export const getUsersWaterRateController = async (req, res, next) => {
+
+  const userId = req.user._id;
+
+   const userById = await getUserWaterRate(userId);
+
+  if (userById === null) {
+    return next(createHttpError('User not found'));
+  }
+
+  res.send({
+    status: 200,
+    message: `User's ${userById.waterRate} WaterRate`,
+    data: userById.waterRate,
+  });
+
+};
+
+export const updateUserPhotoController = async (req, res, next) => {
+
+   const userId = req.user._id;
+
+  const photo = req.file;
+
+  let photoUrl;
+
+  if (photo) {
+    if (env('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
+  }
+
+  const update = await updeteUserPhoto(userId, { photo: photoUrl });
+
+  if (!update) throw createHttpError(404, 'User not found');
+
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully patched a photo',
+    data: update.user.photo,
+  });
+};
+
+export const updateUserInfoController = async (req, res) => {
+
+  const user = req.user;
+  const body = req.body;
+
+  const data = await updateUserInfo(body, user);
+
+  res.json({
+    status: 200,
+    message: 'Successfully patched an user',
+    data,
+  });
+
+};
+
+
--- src/services/users.js
+++ src/services/users.js
@@ -14,3 +14,48 @@ export const getUserProfile = async (userId) => {
     photo: user.photo,
   };
 };
+
+export const getUserWaterRate = async (userId) => {
+
+  const user = await User.findById({ _id: userId });
+
+  return { waterRate: user.waterRate };
+};
+
+export const updeteUserPhoto = async (userId, photo) => {
+
+  const userPhoto = await User.findByIdAndUpdate({ _id: userId }, photo, {
+    new: true,
+    includeResultMetadata: true,
+  });
+
+  if (!userPhoto || !userPhoto.value) return null;
+
+  return {
+    user: userPhoto.value,
+    isNew: Boolean(userPhoto?.lastErrorObject?.upserted),
+  };
+
+};
+
+export const updateUserInfo = async (body, user) => {
+  const newPassword = body.newPassword;
+  const newDailyNorm = body.dailyNorm;
+
+  if (newPassword) {
+    const hashPassword = await bcrypt.hash(newPassword, 10);
+    const data = await User.findOneAndUpdate(user, { ...body, password: hashPassword }, { new: true });
+
+    return { data, };
+  };
+
+  if (newDailyNorm) {
+    const data = await User.findOneAndUpdate(user, { ...body, dailyNorm: newDailyNorm }, { new: true });
+
+    return { data, };
+  }
+
+  const data = await User.findOneAndUpdate(user, { ...body }, { new: true });
+
+  return { data, };
+};
