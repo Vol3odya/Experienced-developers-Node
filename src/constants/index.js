6f1d02e5ced778c55091637338d6f6320bfb5c47
Katrinast <stehnovicekaterina@gmail.com>
2024-12-21 21:15:45 +0200

    changed
    changed constant and deleted requered for user's name

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 import path from 'node:path';
 
-export const FIFTEEN_MINUTES = 15 * 60 * 1000;
-export const ONE_DAY = 24 * 60 * 60 * 1000;
+export const FIFTEEN_MINUTES = 30 * 24 * 60 * 60 * 1000;
+export const ONE_DAY = 30 * 24 * 60 * 60 * 1000;
 
 
 export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
